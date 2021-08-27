f724b8a61e8ffe853fd81268f3c0e0b117552d1b
Felix <felixrunye@gmail.com>
2021-08-27 08:19:58 +0300

    logo and search icon

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -7,10 +8,11 @@ export default function Home() {
         <title>Air Bnb 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* header */}
+      
+      <Header/>
       {/* banner */}
       {/*  */}
-      <h1>Airbnb </h1>
+    
     
 
       
