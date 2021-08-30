05d38909d610b188c563483190658c30790367a1
Felix <felixrunye@gmail.com>
2021-08-30 09:40:34 +0300

    large text complete

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import LargeCard from '../components/LargeCard';
 import MediumCard from '../components/MediumCard';
 import SmallCard from '../components/SmallCard';
 
@@ -21,31 +22,31 @@ export default function Home({ exploreData, cardsData}) {
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          { exploreData?.map((item, key) => (
-            <SmallCard className="flex items-center" key={key} item={item} />
-          ) )}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            { exploreData?.map((item, key) => (
+              <SmallCard className="flex items-center" key={key} item={item} />
+            ) )}
 
-        </div>
-       
+          </div>
         </section>
-            <h2 className="text-4xl front-semibold py-8">Live Anywhere</h2>
-            
-           <div className="flex space-x-2 overflow-scroll scrollbar-hide p-3 ml-2">
+
+        <section>
+          <h2 className="text-4xl front-semibold py-8">Live Anywhere</h2>
+              
+            <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 ml-2">
             { cardsData?.map((item, key)=>(
                 <MediumCard key={key} item={item}/>
               ))}
-           </div>
-
+            </div>
+        </section>
+        
         <section>
-
+          <LargeCard img="https://links.papareact.com/4cj" title="The greatest outdoors"
+          description="wishlist created by airbnb" buttonText="check me out" />
         </section>
 
       </main>
     
-    
-
-      
     </div>
   )
 }
