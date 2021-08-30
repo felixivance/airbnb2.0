ac68e81cb4f2a374c3d82f3dfb6aee4c385cde25
Felix <felixrunye@gmail.com>
2021-08-30 10:07:03 +0300

    footer styling

--- components/Footer.js
+++ components/Footer.js
@@ -1,41 +1,44 @@
 function Footer() {
     return (
     
-              <div className=" grid grid-cols-2 md:grid-cols-4 bg-gray-100 gap-y-10
-               px-32 py-14 text-gray-600 ">
-                <div className="space-y-4 text-gray-800"> 
-                    <h5 className="font-bold"> About </h5> 
-                    <p>How it works</p>
-                    <p>News Room</p>
-                    <p>Investors</p>
-                    <p>Plus</p>
-                    <p>About Felix</p>
-                </div>
-                <div className="space-y-4 text-gray-800"> 
-                    <h5 className="font-bold"> About 2</h5> 
-                    <p>How it works</p>
-                    <p>News Room</p>
-                    <p>Investors</p>
-                    <p>Plus</p>
-                    <p>About Felix</p>
-                </div>
-                <div className="space-y-4 text-gray-800"> 
-                    <h5 className="font-bold"> About 3</h5> 
-                    <p>How it works</p>
-                    <p>News Room</p>
-                    <p>Investors</p>
-                    <p>Plus</p>
-                    <p>About Felix</p>
-                </div>
-                <div className="space-y-4 text-gray-800"> 
-                    <h5 className="font-bold"> About 4</h5> 
-                    <p>How it works</p>
-                    <p>News Room</p>
-                    <p>Investors</p>
-                    <p>Plus</p>
-                    <p>About Felix</p>
-                </div>
-            </div>
+             <div>
+                  <div className=" grid grid-cols-2 md:grid-cols-4 bg-gray-100 gap-y-10
+                    px-32 py-14 text-gray-600 ">
+                        <div className="space-y-4 text-gray-800"> 
+                            <h5 className="font-bold"> About </h5> 
+                            <p>How it works</p>
+                            <p>News Room</p>
+                            <p>Investors</p>
+                            <p>Plus</p>
+                            <p>About Felix</p>
+                        </div>
+                        <div className="space-y-4 text-gray-800"> 
+                            <h5 className="font-bold"> About 2</h5> 
+                            <p>How it works</p>
+                            <p>News Room</p>
+                            <p>Investors</p>
+                            <p>Plus</p>
+                            <p>About Felix</p>
+                        </div>
+                        <div className="space-y-4 text-gray-800"> 
+                            <h5 className="font-bold"> About 3</h5> 
+                            <p>How it works</p>
+                            <p>News Room</p>
+                            <p>Investors</p>
+                            <p>Plus</p>
+                            <p>About Felix</p>
+                        </div>
+                        <div className="space-y-4 text-gray-800"> 
+                            <h5 className="font-bold"> About 4</h5> 
+                            <p>How it works</p>
+                            <p>News Room</p>
+                            <p>Investors</p>
+                            <p>Plus</p>
+                            <p>About Felix</p>
+                        </div>
+                    </div>  
+                    <p className="flex justify-center bg-gray-100 text-md">Developed by Felix: Air bnb Version</p>
+             </div>
         
     )
 }
