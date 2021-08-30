7d5e37cdd29b5fbb8164ec2226771bd11c214217
Felix <felixrunye@gmail.com>
2021-08-30 11:33:13 +0300

    implementing date picker

--- components/Banner.js
+++ components/Banner.js
@@ -7,7 +7,7 @@ function Banner() {
             <Image src="https://links.papareact.com/0fm" layout="fill"
             objectFit="cover" />
             <div className=" absolute top-1/2 w-full text-center">
-                <p className="text-sm sm:text-lg">Kicks of a dying horse</p>
+                <p className="text-sm sm:text-lg">Not sure where to go? Perfect</p>
                 <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full f
                 ont-bold my-3 hover:shadow-xl active:scale-90 transition duration-150" >
                     Check Me Out</button>
--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,26 @@
 import Image from 'next/image';
 import { GlobeAltIcon, MenuIcon, SearchIcon, UserCircleIcon } from '@heroicons/react/outline';
+import { useState } from 'react';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 function Header() {
+    const [searchInput, setSearchInput] = useState("")
+    const [startDate, setStartDate] = useState( new Date());
+    const [endDate, setEndDate] = useState( new Date());
+
+    const selectionRanges ={
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection'
+    }
+
+    // const functionName =()=>{}
+    function handleDateChange(ranges){
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
+    }
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5
         md:px-10">
@@ -16,7 +35,7 @@ function Header() {
             </div>
              {/* search bar */}
              <div className="flex items-center rounded-full md:border-2 md:shadow-md">
-                    <input className="flex-grow pl-4 bg-transparent outline-none text-gray-600 placeholder-gray-400" type="text" placeholder="start your search" />
+                    <input value={searchInput} onChange={(e)=> setSearchInput(e.target.value)} className="flex-grow pl-4 bg-transparent outline-none text-gray-600 placeholder-gray-400" type="text" placeholder="start your search" />
                     <SearchIcon className="hidden md:inline-flex
                      h-8 bg-red-500 text-white rounded-full p-2 cursor-pointer" />
                 </div>
@@ -31,7 +50,17 @@ function Header() {
                         <UserCircleIcon className="h-6" />
                     </div>
                 </div>
+
+            {searchInput && (
+                <div className="flex flex-col-3 col-span-3 mx-auto">
+                    <DateRangePicker ranges={[selectionRanges]} minDate={ new Date() } 
+                    rangeColors={['#FD5B61']} onChange={handleDateChange} />
+                </div>
+            )}
+
         </header>
+
+
     )
 }
 
