bbbcea6ad47890b10f621dae64393f97638be876
Felix <felixrunye@gmail.com>
2021-09-02 11:27:37 +0300

    added pointers to show markers on the map and animate

--- components/Map.js
+++ components/Map.js
@@ -1,10 +1,22 @@
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import getCenter from 'geolib/es/getCenter';
 
-function Map() {
-     const [viewport, setViewport] = useState({
-            latitude: 37.7577,
-            longitude: -122.4376,
+function Map({searchResults}) {
+     
+
+        //transform search results into the required format
+
+        const cordinates = searchResults.map((item)=>({
+            longitude:item.long,
+            latitude:item.lat
+        }));
+        //get center of the cordinates
+        const center = getCenter(cordinates);
+
+        const [viewport, setViewport] = useState({
+            latitude: center.longitude,
+            longitude: center.latitude,
             zoom: 8,
             width:"100%",
             height:"100%"
@@ -12,10 +24,18 @@ function Map() {
         
     return <ReactMapGL 
     mapStyle="mapbox://styles/felixivance/ckt2jw2qc22g218s0qm5shc1l"
-    mapboxApiAccessToken={process.env.mapbox_key}
-         {...viewport}
-      onViewportChange={(viewport) => setViewport(viewport)}
+            mapboxApiAccessToken={process.env.mapbox_key}
+               {...viewport}
+             onViewportChange={(viewport) => setViewport(viewport)}
       >
+          {searchResults.map((result,key)=> (
+                <div key={key}>
+                    <Marker longitude={result.long} latitude={result.lat} offsetLeft={-20}
+                    offsetTop={-10} >
+                        <p className="cursor-pointer text-2xl animate-bounce">📌</p>
+                    </Marker>
+                </div>
+           ))}
 
     </ReactMapGL>;
 }
--- pages/search.js
+++ pages/search.js
@@ -41,7 +41,7 @@ function Search({searchResults}) {
 
                 </section>
                 <section className="hidden xl:inline-flex xl:min-w-[600px]">
-                    <Map />
+                    <Map searchResults={searchResults} />
                 </section>
             </main>
 
